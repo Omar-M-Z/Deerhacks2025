f34d10b6889a94aa657a8aad00c948e5b9fe6a29
omarzaheer <omar.m.zaheer@gmail.com>
2025-02-15 11:01:48 -0500

    added requirements.txt

--- database_upload/main.py
+++ database_upload/main.py
@@ -1,11 +1,14 @@
 import requests
 from neo4j import GraphDatabase
 
+
 def create_node(driver, paper):
+    # retreiving information for each paper
     paper_title = paper["display_name"]
     paper_doi = paper["doi"]
     paper_id = paper["id"]
     summary = driver.execute_query(
+    # creating a node in database
     "CREATE (:Paper {title: $title, doi: $doi, id: $id})",
         title=paper_title,
         doi=paper_doi,
@@ -15,15 +18,17 @@ def create_node(driver, paper):
 
 # connecting to database here
 URI = "bolt://localhost:7687/"
-AUTH = ("neo4j", "dublin-poetic-texas-trust-libra-1811")
+AUTH = ("neo4j", "dublin-poetic-texas-trust-libra-1811") #TODO: ADD YOUR OWN DATABASE PASSWORD HERE
 
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
     print("Successfully connected to database")
 
+    # getting papers from database
     response = requests.get("https://api.openalex.org/works?sample=20&select=display_name,doi,id,referenced_works")
     data = response.json()
     print(data["results"])
 
+    # creating a node for each paper
     for paper in data["results"]:
         create_node(driver, paper)
